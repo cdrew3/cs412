afd8c183e89ca7d49b2d8b2c411309eca8a6eaf0
Chris Drew <cdrew3@illinois.edu>
2015-09-21 21:36:47 -0500

    Finished HW and reorganized structure to match HW requirements

---
+++ assignment_1/hw1.cdrew3/question4c_cdrew3.m
@@ -0,0 +1,16 @@
+clc;
+
+x = [.69, -1.31, .39, .05, 1.29, .49, .19, -.81, -.31, .71];
+y = [.89, -1.11, .59, .45, 1.19, .69, .25, -.71, -.21, .71];
+
+xMean = mean(x);
+yMean = mean(y);
+xNum = length(x);
+
+for i = 1:numel(x)
+    xM(i) = (x(i) - xMean); % Zero Mean
+    yM(i) = (y(i) - yMean); % Zero Mean
+end
+
+xy = [xM; yM];
+xyCov = (1/(xNum-1))*xy*xy'
